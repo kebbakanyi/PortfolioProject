e12d058cff35206b49151ea7c4282ba2afaa9e35
Kebba Kanyi <kebbakanyi@Kebbas-MacBook-Pro.local>
2018-06-28 00:34:09 -0700

    .

--- portfolio-site/portfolio/settings.py
+++ portfolio-site/portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'jassong1',
+        'PASSWORD': 'somepassword',
         'HOST': 'localhost',
         'PORT': '5432',
     }
